5dc1eec3fea9f628369bfc2c5ccb9cc21cf0f3b3
Jacob Harvey <jacoblharvey@outlook.com>
2023-08-04 08:09:51 -0500

    Format majors list and add affinity property

--- src/majorsData.js
+++ src/majorsData.js
@@ -3,26 +3,126 @@
 // for us to be able to work on it easier as a team.
 //
 export const availableMajors = [
-    {id: 1, name: 'Computer Science', description: 'computer systems and programming' },
-    {id: 2, name: 'Psychology', description: 'computer systems and programing' },
-    {id: 3, name: 'Business Admin', description: 'computer systems and programing' },
-    {id: 4, name: 'Mechanical Eng', description:'computer systems and programing' },
-    {id: 5, name: 'Biology', description: 'computer systems and programing' },
-    {id: 6, name: 'Art Hist', description: 'computer systems and programing' },
-    {id: 7, name: 'Environmental Sci', description: 'computer systems and programing' },
-    {id: 8, name: 'Journalism', description: 'computer systems and programing' },
-    {id: 9, name: 'Marketing', description: 'computer systems and programing' },
-    {id: 10, name: 'English Lit', description: 'computer systems and programing' },
-    {id: 11, name: 'Civil Eng', description: 'computer systems and programing' },
-    {id: 12, name: 'Political Sc', description: 'computer systems and programing' },
-    {id: 13, name: 'Chemistry', description: 'computer systems and programing'},
-    {id: 14, name: 'Music Perf', description: 'computer systems and programing' },
-    {id: 15, name: 'Economics', description: 'computer systems and programing' },
-    {id: 16, name: 'Sociology', description: 'computer systems and programing' },
-    {id: 17, name: 'Nursing', description: 'computer systems and programing' },
-    {id: 18, name: 'Graphics', description: 'computer systems and programing'},
-    {id: 19, name: 'Physics', description: 'computer systems and programing' },
-    {id: 20, name: 'History', description: 'computer systems and programing' },    
+  {
+    id: 1, 
+    name: 'Computer Science', 
+    description: 'computer systems and programming', 
+    affinity: 10, 
+  },
+  {
+    id: 2, 
+    name: 'Psychology', 
+    description: 'computer systems and programing', 
+    affinity: 10, 
+  },
+  {
+    id: 3, 
+    name: 'Business Administration', 
+    description: 'computer systems and programing', 
+    affinity: 10,
+  },
+  {
+    id: 4, 
+    name: 'Mechanical Engineering', 
+    description:'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 5, 
+    name: 'Biology', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 6, 
+    name: 'Art History', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 7, 
+    name: 'Environmental Science', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 8, 
+    name: 'Journalism', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 9, 
+    name: 'Marketing', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 10, 
+    name: 'English Literature', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 11, 
+    name: 'Civil Engineering', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 12, 
+    name: 'Political Science', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 13, 
+    name: 'Chemistry', 
+    description: 'computer systems and programing',
+    affinity: 10
+  },
+  {
+    id: 14, 
+    name: 'Music Performance', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 15, 
+    name: 'Economics', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 16, 
+    name: 'Sociology', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 17, 
+    name: 'Nursing', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 18, 
+    name: 'Graphic Design', 
+    description: 'computer systems and programing',
+    affinity: 10
+  },
+  {
+    id: 19, 
+    name: 'Physics', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  },
+  {
+    id: 20, 
+    name: 'History', 
+    description: 'computer systems and programing' ,
+    affinity: 10
+  }, 
   ];
   
   export const questionsToAsk = [
