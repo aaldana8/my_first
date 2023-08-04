194de2d50323dbf8b17e4b3914a2e5ca776d10a0
Jacob Harvey <jacoblharvey@outlook.com>
2023-08-04 11:07:38 -0500

    Update majors and questions to mimic concept quiz
    These will all need to be changed once we've decided which majors we're
    including and what questions we want to ask. For now, this structure
    mimics the proof of concept I pushed earlier.

--- src/majorsData.js
+++ src/majorsData.js
@@ -2,6 +2,8 @@
 // majorsData.js is housing the queestions/major/descriptions. Adds more space for my MajorChooser.js and breaks down my project even more
 // for us to be able to work on it easier as a team.
 //
+
+// TODO: Add rest of majors after web scraping
 export const availableMajors = [
   {
     id: 1, 
@@ -51,131 +53,132 @@ export const availableMajors = [
     description: 'computer systems and programing' ,
     affinity: 10
   },
-  {
-    id: 9, 
-    name: 'Marketing', 
-    description: 'computer systems and programing' ,
-    affinity: 10
-  },
-  {
-    id: 10, 
-    name: 'English Literature', 
-    description: 'computer systems and programing' ,
-    affinity: 10
-  },
-  {
-    id: 11, 
-    name: 'Civil Engineering', 
-    description: 'computer systems and programing' ,
-    affinity: 10
-  },
-  {
-    id: 12, 
-    name: 'Political Science', 
-    description: 'computer systems and programing' ,
-    affinity: 10
-  },
-  {
-    id: 13, 
-    name: 'Chemistry', 
-    description: 'computer systems and programing',
-    affinity: 10
-  },
-  {
-    id: 14, 
-    name: 'Music Performance', 
-    description: 'computer systems and programing' ,
-    affinity: 10
-  },
-  {
-    id: 15, 
-    name: 'Economics', 
-    description: 'computer systems and programing' ,
-    affinity: 10
-  },
-  {
-    id: 16, 
-    name: 'Sociology', 
-    description: 'computer systems and programing' ,
-    affinity: 10
-  },
-  {
-    id: 17, 
-    name: 'Nursing', 
-    description: 'computer systems and programing' ,
-    affinity: 10
-  },
-  {
-    id: 18, 
-    name: 'Graphic Design', 
-    description: 'computer systems and programing',
-    affinity: 10
-  },
-  {
-    id: 19, 
-    name: 'Physics', 
-    description: 'computer systems and programing' ,
-    affinity: 10
-  },
-  {
-    id: 20, 
-    name: 'History', 
-    description: 'computer systems and programing' ,
-    affinity: 10
-  }, 
+  // {
+  //   id: 9, 
+  //   name: 'Marketing', 
+  //   description: 'computer systems and programing' ,
+  //   affinity: 10
+  // },
+  // {
+  //   id: 10, 
+  //   name: 'English Literature', 
+  //   description: 'computer systems and programing' ,
+  //   affinity: 10
+  // },
+  // {
+  //   id: 11, 
+  //   name: 'Civil Engineering', 
+  //   description: 'computer systems and programing' ,
+  //   affinity: 10
+  // },
+  // {
+  //   id: 12, 
+  //   name: 'Political Science', 
+  //   description: 'computer systems and programing' ,
+  //   affinity: 10
+  // },
+  // {
+  //   id: 13, 
+  //   name: 'Chemistry', 
+  //   description: 'computer systems and programing',
+  //   affinity: 10
+  // },
+  // {
+  //   id: 14, 
+  //   name: 'Music Performance', 
+  //   description: 'computer systems and programing' ,
+  //   affinity: 10
+  // },
+  // {
+  //   id: 15, 
+  //   name: 'Economics', 
+  //   description: 'computer systems and programing' ,
+  //   affinity: 10
+  // },
+  // {
+  //   id: 16, 
+  //   name: 'Sociology', 
+  //   description: 'computer systems and programing' ,
+  //   affinity: 10
+  // },
+  // {
+  //   id: 17, 
+  //   name: 'Nursing', 
+  //   description: 'computer systems and programing' ,
+  //   affinity: 10
+  // },
+  // {
+  //   id: 18, 
+  //   name: 'Graphic Design', 
+  //   description: 'computer systems and programing',
+  //   affinity: 10
+  // },
+  // {
+  //   id: 19, 
+  //   name: 'Physics', 
+  //   description: 'computer systems and programing' ,
+  //   affinity: 10
+  // },
+  // {
+  //   id: 20, 
+  //   name: 'History', 
+  //   description: 'computer systems and programing' ,
+  //   affinity: 10
+  // }, 
 ];
   
+// TODO: Add rest of questions after web scraping
 export const questionsToAsk = [
   {
     id: 1, 
     text: 'I need the freedom to be creative and express myself.', 
     weights: [
       1, // Computer Science
-      1, // Psychology
-      1, // Business Administration
+      0, // Psychology
+      0, // Business Administration
       1, // Mechanical Engineering
-      1, // Biology
-      1, // Art History
-      1, // Environmental Science
-      1, // Journalism
-      1, // Marketing
-      1, // English Literature
-      1, // Civil Engineering
-      1, // Political Science
-      1, // Chemistry
-      1, // Music Performance
-      1, // Economics
-      1, // Sociology
-      1, // Nursing
-      1, // Graphic Design
-      1, // Physics
-      1, // History
+      0, // Biology
+      2, // Art History
+      2, // Environmental Science
+      3, // Journalism
+      // 1, // Marketing
+      // 1, // English Literature
+      // 1, // Civil Engineering
+      // 1, // Political Science
+      // 1, // Chemistry
+      // 1, // Music Performance
+      // 1, // Economics
+      // 1, // Sociology
+      // 1, // Nursing
+      // 1, // Graphic Design
+      // 1, // Physics
+      // 1, // History
     ], 
   },
   { 
     id: 2, 
     text: 'I care about the state of the environment and want to help improve it in my career.', 
     weights: [
-      1, // Computer Science
-      1, // Psychology
-      1, // Business Administration
-      1, // Mechanical Engineering
-      1, // Biology
-      1, // Art History
-      1, // Environmental Science
-      1, // Journalism
-      1, // Marketing
-      1, // English Literature
-      1, // Civil Engineering
-      1, // Political Science
-      1, // Chemistry
-      1, // Music Performance
-      1, // Economics
-      1, // Sociology
-      1, // Nursing
-      1, // Graphic Design
-      1, // Physics
-      1, // History
+      0, // Computer Science
+      0, // Psychology
+      0, // Business Administration
+      0, // Mechanical Engineering
+      2, // Biology
+      0, // Art History
+      3, // Environmental Science
+      0, // Journalism
+      // 1, // Marketing
+      // 1, // English Literature
+      // 1, // Civil Engineering
+      // 1, // Political Science
+      // 1, // Chemistry
+      // 1, // Music Performance
+      // 1, // Economics
+      // 1, // Sociology
+      // 1, // Nursing
+      // 1, // Graphic Design
+      // 1, // Physics
+      // 1, // History
     ], 
   },
   { 
@@ -183,25 +186,81 @@ export const questionsToAsk = [
     text: 'I like to give advice.', 
     weights: [
       1, // Computer Science
-      1, // Psychology
-      1, // Business Administration
+      3, // Psychology
+      0, // Business Administration
       1, // Mechanical Engineering
-      1, // Biology
-      1, // Art History
-      1, // Environmental Science
+      0, // Biology
+      0, // Art History
+      0, // Environmental Science
+      2, // Journalism
+      // 1, // Marketing
+      // 1, // English Literature
+      // 1, // Civil Engineering
+      // 1, // Political Science
+      // 1, // Chemistry
+      // 1, // Music Performance
+      // 1, // Economics
+      // 1, // Sociology
+      // 1, // Nursing
+      // 1, // Graphic Design
+      // 1, // Physics
+      // 1, // History
+    ], 
+  },
+  { 
+    id: 4, 
+    text: 'I am interested in rehabilitating and healing people.', 
+    weights: [
+      0, // Computer Science
+      3, // Psychology
+      0, // Business Administration
+      0, // Mechanical Engineering
+      2, // Biology
+      0, // Art History
+      0, // Environmental Science
+      0, // Journalism
+    ], 
+  },
+  { 
+    id: 5, 
+    text: 'I am very ambitious, persuasive, and love coming up with new ideas.', 
+    weights: [
+      1, // Computer Science
+      0, // Psychology
+      3, // Business Administration
+      0, // Mechanical Engineering
+      0, // Biology
+      0, // Art History
+      2, // Environmental Science
+      2, // Journalism
+    ], 
+  },
+  { 
+    id: 6, 
+    text: 'I enjoy learning about different parts of the world.', 
+    weights: [
+      0, // Computer Science
+      0, // Psychology
+      2, // Business Administration
+      0, // Mechanical Engineering
+      0, // Biology
+      3, // Art History
+      2, // Environmental Science
       1, // Journalism
-      1, // Marketing
-      1, // English Literature
-      1, // Civil Engineering
-      1, // Political Science
-      1, // Chemistry
-      1, // Music Performance
-      1, // Economics
-      1, // Sociology
-      1, // Nursing
-      1, // Graphic Design
-      1, // Physics
-      1, // History
+    ], 
+  },
+  { 
+    id: 7, 
+    text: 'I like math and figuring out how things work.', 
+    weights: [
+      3, // Computer Science
+      0, // Psychology
+      0, // Business Administration
+      3, // Mechanical Engineering
+      2, // Biology
+      0, // Art History
+      0, // Environmental Science
+      0, // Journalism
     ], 
   },
 ];
